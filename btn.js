b653141f3d10be0003f970643d995ccf105ea202
Emanuel <emanuel.casellero@gmail.com>
2022-11-09 21:09:19 -0300

    cambio

--- btn.js
+++ btn.js
@@ -3,3 +3,8 @@ let mainNav = document.getElementById('main-nav');
 btnMenu.addEventListener('click', function(){
   mainNav.classList.toggle('mostrar');
 });
+const sub = document.getElementById("subb");
+
+sub.addEventListener("submit", function(event) {
+   event.preventDefault();
+})
